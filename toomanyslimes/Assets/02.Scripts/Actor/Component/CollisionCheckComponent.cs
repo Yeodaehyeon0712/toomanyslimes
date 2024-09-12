a805682d02c05d5a154a75bd984255ad7d378e4b
Yeo <yeodaehyeon@gmail.com>
2024-09-12 23:04:04 +0900

    CollisionCheckComponent 코드 최적화 및 전방 공격 대상 체크 메서드 생성

--- toomanyslimes/Assets/02.Scripts/Actor/Component/CollisionCheckComponent.cs
+++ toomanyslimes/Assets/02.Scripts/Actor/Component/CollisionCheckComponent.cs
@@ -26,7 +26,7 @@ public class CollisionCheckComponent : BaseComponent
         CheckRight();
     }
     #endregion
-    GameObject a;
+
     #region Check Method  
     public void CheckLeft()
     {
@@ -53,38 +53,44 @@ public class CollisionCheckComponent : BaseComponent
         bool collisionDetected = CheckRaycastCollisions(direction1, direction2, direction3,0.1f);
         BackgroundManager.Instance.IsBGMove = (collisionDetected==false);
     }
-    bool CheckRaycastCollisions(Vector2 dir1,Vector2 dir2,Vector2 dir3,float extraLength=0)
+    bool CheckRaycastCollisions(Vector2 dir1, Vector2 dir2, Vector2 dir3, float extraLength = 0)
     {
         Vector2 basePosition = ownerTransform.position;
+        Vector2[] directions = { dir1, dir2, dir3 };
+        float[] lengths = { rayLength + extraLength, rayLength, rayLength + extraLength };
 
-        RaycastHit2D hit1 = Physics2D.Raycast(basePosition, dir1, rayLength+extraLength, obstacleLayer);
-        RaycastHit2D hit2 = Physics2D.Raycast(basePosition, dir2, rayLength, obstacleLayer);
-        RaycastHit2D hit3 = Physics2D.Raycast(basePosition, dir3, rayLength + extraLength, obstacleLayer);
-
-
-        bool touched = (hit1.collider != null && (hit1.collider.CompareTag("Enemy") || hit1.collider.CompareTag("Obstacle"))) ||
-                       (hit2.collider != null && (hit2.collider.CompareTag("Enemy") || hit2.collider.CompareTag("Obstacle"))) ||
-                       (hit3.collider != null && (hit3.collider.CompareTag("Enemy") || hit3.collider.CompareTag("Obstacle")));
+        for (int i = 0; i < 3; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(basePosition, directions[i], lengths[i], obstacleLayer);
 #if UNITY_EDITOR
-        Debug.DrawRay(basePosition, dir1 * (rayLength + extraLength), Color.blue);
-        Debug.DrawRay(basePosition, dir2 * rayLength, Color.blue);
-        Debug.DrawRay(basePosition, dir3 * (rayLength + extraLength), Color.blue);
+            Debug.DrawRay(basePosition, directions[i] * lengths[i], Color.blue);
 #endif
-        return touched;
+            if (hit.collider != null && (hit.collider.CompareTag("Enemy") || hit.collider.CompareTag("Obstacle")))
+                return true;
+        }
+        return false;
     }
-    void CheckTarget()
+    GameObject a;
+    void CheckBattleTarget()
     {
-        //if(touched)
-        //    Debug.Log("전면출돌");
+        RaycastHit2D hit = Physics2D.Raycast(ownerTransform.position, Vector2.up, rayLength, obstacleLayer);
+        if (hit.collider == null) return;
 
-        //if(hit2.collider != null&& hit2.collider.CompareTag("Enemy"))
-        //{
-        //    if(a!=hit2.collider.gameObject)
-        //    {
-        //        a = hit2.collider.gameObject;
-        //        Debug.Log("공격");
-        //    }
-        //}
+        //전방에 충돌한 적이 없다면
+        if (hit.collider.CompareTag("Enemy") == false)
+        {
+            //내 상태는 배틀에서 벗어나 무브로 변경
+            a = null;
+            return;
+        }
+
+        //충돌했지만 전에 충돌했던게 아니라면 공격의 대상을 변경
+        if (a != hit.collider.gameObject)
+        {
+            a = hit.collider.gameObject;
+            Debug.Log("공격");
+        }
+
     }
     #endregion
 }
