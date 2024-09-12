a81f75a6cc76d6f3eae21214a00b2376b47f021a
Yeo <yeodaehyeon@gmail.com>
2024-09-13 01:34:18 +0900

    Monster Table 생성

---
+++ toomanyslimes/Assets/02.Scripts/Data/Table/MonsterTable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Data
+{
+    public class MonsterData
+    {
+        public float HP;
+        public float AttackDamage;
+        public float AttackSpeed;
+        public string ResourcePath;
+        public int PathHash;
+        public bool IsBoss;
+        public bool IsRangeAttack;
+
+        public MonsterData(Dictionary<string, string> dataPair)
+        {
+
+            HP = float.Parse(dataPair["HP"]);
+            AttackDamage = float.Parse(dataPair["AttackDamage"]);
+            AttackSpeed = float.Parse(dataPair["AttackSpeed"]);
+            ResourcePath = dataPair["ResourcePath"];
+            PathHash = ResourcePath.GetHashCode();
+            IsBoss = bool.Parse(dataPair["IsBoss"]);
+            IsBoss = bool.Parse(dataPair["IsRangedAttack"]);
+        }
+    }
+}
+
+public class MonsterTable : TableBase
+{
+    Dictionary<long, Data.MonsterData> _monsterDataDic = new Dictionary<long, Data.MonsterData>();
+    public Data.MonsterData this[long index]
+    {
+        get
+        {
+            if (_monsterDataDic.ContainsKey(index))
+                return _monsterDataDic[index];
+
+            return null;
+        }
+    }
+    protected override void OnLoad()
+    {
+        LoadData(_tableName);
+        foreach (var contents in _dataDic)
+            _monsterDataDic.Add(contents.Key, new Data.MonsterData(contents.Value));
+    }
+}
--- toomanyslimes/Assets/02.Scripts/Manager/DataManager.cs
+++ toomanyslimes/Assets/02.Scripts/Manager/DataManager.cs
@@ -7,13 +7,18 @@ public class DataManager : TSingletonMono<DataManager>
 
     public static WaveTable WaveTable;
     public static StageTable StageTable;
+    public static MonsterTable MonsterTable;
 
     protected override void OnInitialize()
     {
         WaveTable = LoadTable<WaveTable>(eTableName.WaveTable);
         WaveTable.Reload();
+
         StageTable = LoadTable<StageTable>(eTableName.StageTable);
         StageTable.Reload();
+
+        MonsterTable = LoadTable<MonsterTable>(eTableName.MonsterTable);
+        MonsterTable.Reload();
 
         IsLoad = true;      
     }
@@ -30,5 +35,6 @@ public enum eTableName
 {
     WaveTable = 1 << 0,
     StageTable = 1 << 1,
+    MonsterTable = 1 << 2,
     All = ~0,
 }
