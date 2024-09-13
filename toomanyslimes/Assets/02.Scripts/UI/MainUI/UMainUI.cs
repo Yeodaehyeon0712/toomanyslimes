fd6041c6506f02d5a13f7ad73df9da72271957d9
Yeo <yeodaehyeon@gmail.com>
2024-09-13 17:18:01 +0900

    Normal / Boss Stage UI 작업

--- toomanyslimes/Assets/02.Scripts/UI/MainUI/UMainUI.cs
+++ toomanyslimes/Assets/02.Scripts/UI/MainUI/UMainUI.cs
@@ -7,26 +7,31 @@ using TMPro;
 public class UMainUI : UBaseUI
 {
     #region Field
-    Button btn_Setting;
-    TextMeshProUGUI text_title;
-    Button btn_LeftModel;
-    Button btn_RightModel;
-    Button btn_Info;
 
+    GameObject panel_normalStage;
+    Button btn_Pause;
+    TextMeshProUGUI text_CoinCount;
+    Slider slider_progress;
+
+    GameObject panel_bossStage;
+    TextMeshProUGUI text_bossHP;
+    Slider slider_bossHP;
     #endregion
 
     #region Init
     protected override void InitReference()
-    {      
-        btn_Setting = transform.Find("Panel_Title/Btn_Setting").GetComponent<Button>();
-        btn_Setting.onClick.AddListener(OpenSetting);
-        btn_LeftModel = transform.Find("Panel_NextModel/Btn_Left").GetComponent<Button>();
-        btn_LeftModel.onClick.AddListener(() => { NextModel(isRight: false); });
-        btn_RightModel = transform.Find("Panel_NextModel/Btn_Right").GetComponent<Button>();
-        btn_RightModel.onClick.AddListener(() => { NextModel(isRight: true); });
+    {
+        panel_normalStage = transform.Find("Panel_NormalStage").gameObject;
+        btn_Pause = transform.Find("Btn_Pause").GetComponent<Button>();
+        btn_Pause.onClick.AddListener(OpenSetting);
+        text_CoinCount = transform.Find("Panel_NormalStage/Panel_Coin/Text_Title").GetComponent<TextMeshProUGUI>();
+        slider_progress = transform.Find("Panel_NormalStage/Slider_Progress").GetComponent<Slider>();
+        slider_progress.value = 0;
 
-        btn_Info = transform.Find("Btn_Info").GetComponent<Button>();
-        btn_Info.onClick.AddListener(OpenDescription); 
+        panel_bossStage= transform.Find("Panel_BossStage").gameObject;
+        text_bossHP= transform.Find("Panel_BossStage/Text_BossHP").GetComponent<TextMeshProUGUI>();
+        slider_progress = transform.Find("Panel_BossStage/Slider_BossHP").GetComponent<Slider>();
+        slider_progress.value = 1f;
     }
 
     protected override void OnRefresh()
@@ -38,15 +43,30 @@ public class UMainUI : UBaseUI
         //이건 enum 형으로 처리할수도 있음 고민 .
         UIManager.Instance.SettingUI.Enable();
     }
-    public void OpenDescription()
+    #region Normal Stage Method
+    public void ShowNormalStagePanel(bool isShow)
     {
-        UIManager.Instance.DescriptionUI.MoveIn(1f);
+        panel_normalStage.SetActive(isShow);
     }
-    #region Model Method
-    void NextModel(bool isRight)
+    public void SetCoinCount(int coinCount)
     {
-        //ModelManager.Instance.NextModel(isRight);
+        text_CoinCount.text = coinCount.ToString();
+    }    
+    public void SetProgressSlider(float progressValue)
+    {
+        slider_progress.value = progressValue;
     }
     #endregion
 
+    #region Boss Stage Method
+    public void ShowBossStagePanel(bool isShow)
+    {
+        panel_bossStage.SetActive(isShow);
+    }
+    public void SetHPSlider(float currentHP,float maxHP)
+    {
+        slider_bossHP.value = currentHP / maxHP;
+        text_bossHP.text = currentHP.ToString();
+    }
+    #endregion
 }
